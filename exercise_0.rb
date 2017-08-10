1d896a7e97188acdf923dbc5a3364cc6c53f9fcc
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-10 16:41:50 -0400

    exercise_0

---
+++ exercise_0.rb
@@ -0,0 +1,13 @@
+# list of different arrays of question 1
+
+favourate_colours = ["red", "green", "yellow", "blue", "white"]
+age_of_me_my_sib_friends = [ 31, 40, 32]
+flipped_coin_result = ["head", "tail", "head", "head", "tail"]
+three_fav_artists = ["Richard Gere", "Tom Cruse", "Amir Khan"]
+favourate_colours_symbols = [:green, :blue, :white]
+
+# list of hash of question 2
+words_def = { :honest => "free of deceit and untruthfulness", :karma => "destiny or fate,following as effect from cause", :ethics => "moral principles that govern a person's behaviour or the conducting of an activity" }
+favourate_movies_year = {"Cast Away" => 2000, "Gladiator" => 2000, "Baby's Day Out" => 1994}
+three_highest_population_city = { "Tokyo" => 37833000 , "Dellhi" => 24953000 , "Shanghai" => 22991000}
+sib_friend_name_age = { "Gonpo" => 40, "Tenzin" => 32}
