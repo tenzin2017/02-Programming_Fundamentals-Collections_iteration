63720e3d048ad55319f06fb2d4616c84fc09a880
tenzin2017 <dolma@Owners-MacBook-Pro.local>
2017-08-13 07:59:12 -0400

    Exercise 11 pizza order program finished

--- exercise10.rb
+++ exercise10.rb
@@ -1,3 +1,4 @@
+#Exercise-10
 num_list = (1..100)
 num_list.each do |num|
   if num % 3 == 0 && num % 5 != 0
@@ -10,3 +11,16 @@ num_list.each do |num|
     puts num
   end
 end
+
+#Exercie-11
+def pizza
+  puts "How many pizzas do you want to order ?"
+  quantity = gets.chomp.to_i
+  quantity.times do |order_no|
+    puts "How many toppings for pizza #{order_no + 1}"
+    topping_quantity = gets.chomp.to_i
+    puts "You have ordered a pizza with #{topping_quantity} toppings"
+  end
+end
+
+pizza
